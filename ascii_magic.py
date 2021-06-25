4d5f8f00c69bdc580bc2ffc3340642786d0d7a52
KAK_ZED <71234040+968hacker@users.noreply.github.com>
2021-06-26 02:37:03 +0300

    Add files via upload

---
+++ ascii_magic.py
@@ -0,0 +1,17 @@
+import  os 
+os.system("clear ;pkg install jp2a ;clear")
+s= '''
+              c0000000Oc
+              Cdlkk0000O
+         .oxxxxxxxx0000O kkd'
+        .0KKK0000000000d 0KK0.
+        ,KKKKOllooooooll00KKK,
+        .0KKK oxxkkkOOO000KK0.
+         .oxx xxxkkooooddddc.
+              xxxkkxkld
+              Cdxkkkkc0o
+'''
+print(s)
+print("===================================")
+file=input("[ Path ] File ? >> ")
+print("===================================")
